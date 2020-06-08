36e3db646a25dcf56121e9997c2a0dbb58441562
Arun19021987 <57087397+Arun19021987@users.noreply.github.com>
2020-06-08 14:59:51 +0530

    Update and rename Build.groovy to Build.bat

---
+++ Build.bat
@@ -0,0 +1,1 @@
+C:/tibco/tra/5.7/bin/buildear -v -x -s -ear /Deployment/JenkinsDemo.archive -o C:/Program Files (x86)/Jenkins/workspace/JenkinsAutomation/JenkinsDemo.ear -p C:/Program Files (x86)/Jenkins/workspace/JenkinsAutomation/JenkinsDemo
