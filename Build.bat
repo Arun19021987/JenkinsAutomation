20435855deeb3b25f8dd26957ec8f48a92e35677
Arun19021987 <57087397+Arun19021987@users.noreply.github.com>
2020-06-08 16:10:18 +0530

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,1 +1,1 @@
-C:/tibco/tra/5.7/bin/buildear -v -x -s -ear /Deployment/JenkinsDemo.archive -o C:/Program Files (x86)/Jenkins/workspace/JenkinsAutomation/JenkinsDemo.ear -p C:/Program Files (x86)/Jenkins/workspace/JenkinsAutomation/JenkinsDemo
+C:/tibco/tra/5.7/bin/buildear -v -x -s -ear /Deployment/JenkinsDemo.archive -o F:/Jenkins/JenkinsDemo.ear -p F:/Jenkins/JenkinsDemo
